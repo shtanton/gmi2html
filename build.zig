eb2ed5ead279209424264181cfd74a4b895f4c1a
Charlie Stanton <charlie@shtanton.com>
2021-01-07 20:56:05 +0000

    Build is now single threaded

--- build.zig
+++ build.zig
@@ -10,6 +10,7 @@ pub fn build(b: *Builder) void {
     exe.setTarget(target);
     exe.setBuildMode(mode);
     exe.strip = strip;
+    exe.single_threaded = true;
     exe.install();
 
     const run_cmd = exe.run();
